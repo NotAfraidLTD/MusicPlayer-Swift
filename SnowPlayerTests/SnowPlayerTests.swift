bb828764f7966f33602ab99372562c3f22a78972
codeWorm2015 <liu_rui_long@163.com>
2015-09-24 10:46:23 +0800

    Initial Commit

---
+++ SnowPlayerTests/SnowPlayerTests.swift
@@ -0,0 +1,36 @@
+//
+//  SnowPlayerTests.swift
+//  SnowPlayerTests
+//
+//  Created by 刘瑞龙 on 15/9/24.
+//  Copyright (c) 2015年 刘瑞龙. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SnowPlayerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
